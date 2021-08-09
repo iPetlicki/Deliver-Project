1cfd4d7b4ff3a12ed602b831d199af3a6ead73f0
iPetlicki <petlicki.igor@gmail.com>
2021-08-10 00:02:59 +0300

    about

---
+++ JS/about.js
@@ -0,0 +1,12 @@
+// =================================burger
+let nav = $("#nav");
+let slide = $("#slide")
+let navToggle = $("#navToggle");
+navToggle.on("click", function (event) {
+    event.preventDefault();
+    nav.toggleClass("show");
+})
+navToggle.on("click", function (event) {
+    event.preventDefault();
+    slide.toggleClass("hid");
+});
